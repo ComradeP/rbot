09761d472805eaaa21ad86078a287c2bd706cdb7
Voker57 <voker57@gmail.com>
2009-12-12 21:49:33 +0300

    markov: use not only first two first words for building phrase

--- data/rbot/plugins/markov.rb
+++ data/rbot/plugins/markov.rb
@@ -505,6 +505,16 @@ class MarkovPlugin < Plugin
     1 + rand(5)
   end
 
+  # Generates all sequence pairs from array
+  # seq_pairs [1,2,3,4] == [ [1,2], [2,3], [3,4]]
+  def seq_pairs(arr)
+    res = []
+    0.upto(arr.size-2) do |i|
+      res << [arr[i], arr[i+1]]
+    end
+    res
+  end
+
   def set_delay(m, params)
     case params[:delay]
     when "on"
@@ -532,14 +542,34 @@ class MarkovPlugin < Plugin
   def random_markov(m, message)
     return unless should_talk
 
-    word1, word2 = clean_str(message).split(/\s+/)
-    return unless word1 and word2
-    line = generate_string(word1.intern, word2.intern)
-    return unless line
-    # we do nothing if the line we return is just an initial substring
-    # of the line we received
-    return if message.index(line) == 0
-    reply_delay m, line
+    words = clean_str(message).split(/\s+/)
+    if words.length < 2
+      line = generate_string words.first, nil
+
+      if line
+    		return if message.index(line) == 0
+    		reply_delay m, line
+			return
+		end
+	 else
+		pairs = seq_pairs(words).sort_by { rand }
+		pairs.each do |word1, word2|
+			line = generate_string(word1.intern, word2.intern)
+# 			warning "pair is #{word1.inspect}, #{word2.inspect}"
+# 			warning "line is #{line.inspect}"
+			if line and message.index(line) != 0
+				reply_delay m, line
+				return
+			end
+		end
+		words.sort_by { rand }.each do |word|
+			line = generate_string word.first, nil
+			if line and message.index(line) != 0
+				reply_delay m, line
+				return
+			end
+		end
+    end
   end
 
   def chat(m, params)
